adc17ca2f31e233cb30fbc699daa74f4d654476e
Anna Nguyen <annanguyn99@gmail.com>
2024-12-29 23:32:37 +1100

    Added login function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, abort
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -12,6 +12,21 @@ def index():
 def signup():
     return render_template("signup.html")
 
+# Handle post request from login page, send back a URL to home page
+@app.route('/login/user', methods=['POST'])
+def handle_login():
+    if not request.is_json:
+        abort(404)
+
+    username = request.json.get("username")
+    password = request.json.get("password")
+    print(username, password)
+
+    return url_for("home")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
